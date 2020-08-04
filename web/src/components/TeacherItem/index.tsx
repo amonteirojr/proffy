6ecb0d50cedfa7deeece60839dfa67f84236f950
Adriano Monteiro Jr <amonteirojr@gmail.com>
2020-08-03 21:26:18 -0300

    Primeiro commit com algumas páginas web

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
+
+import "./styles.css";
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://avatars0.githubusercontent.com/u/12853061?s=400&u=650832da78ac3b345f311c318611676047e156b0&v=4"
+          alt="Usuário"
+        />
+        <div>
+          <strong>Adriano Monteiro Jr.</strong>
+          <span>Química</span>
+        </div>
+      </header>
+
+      <p>
+        Entusiasta das melhores tecnologias de química avançada.
+        <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export default TeacherItem;
