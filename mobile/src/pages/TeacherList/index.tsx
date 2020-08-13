325978af57a7a5c51101e4efe9fe8c2cce026e41
Adriano Monteiro Jr <amonteirojr@gmail.com>
2020-08-12 23:57:25 -0300

    Finalizando app mobile

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import { View, ScrollView, Text, TextInput } from "react-native";
 import { BorderlessButton, RectButton } from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-community/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
 import api from "../../services/api";
 
@@ -34,6 +35,10 @@ function TeacherList() {
       }
     });
   }
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
 
   function handleToggleFiltersVisible() {
     setAreFiltersVisible(!areFiltersVisible);
